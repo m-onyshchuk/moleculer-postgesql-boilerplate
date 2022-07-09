6b127a14909737eb6439843ca7d8952304d6a958
N.Onishchuk <n.a.onischyuk@diacare-soft.ru>
2022-07-09 22:06:45 +0300

    ~ migrations

--- migrations/01-create-tables.sql
+++ migrations/01-create-tables.sql
@@ -1,6 +1,28 @@
 DO
 $do$
-    BEGIN
+  BEGIN
 
-	END
+    CREATE TABLE IF NOT EXISTS public.authors (
+      id SERIAL,
+      first_name TEXT NOT NULL,
+      last_name TEXT NOT NULL,
+      CONSTRAINT authors_pkey PRIMARY KEY (id)
+    );
+
+    CREATE TABLE IF NOT EXISTS public.publishers (
+      id SERIAL,
+      name TEXT NOT NULL,
+      CONSTRAINT publishers_pkey PRIMARY KEY (id)
+    );
+
+    CREATE TABLE IF NOT EXISTS public.books (
+      id SERIAL,
+      name TEXT NOT NULL,
+      isbn TEXT NOT NULL,
+      author int REFERENCES authors (id),
+      publisher int REFERENCES publishers (id),
+      CONSTRAINT books_pkey PRIMARY KEY (id)
+    );
+
+  END
 $do$;
--- migrations/02-seed-tables.sql
+++ migrations/02-seed-tables.sql
@@ -1,6 +1,32 @@
 DO
 $do$
-	BEGIN
+ DECLARE
+   records int;
+ BEGIN
 
-	END
+  -- check authors
+  SELECT COUNT(*) INTO records FROM public.authors;
+  IF records = 0 THEN
+    INSERT INTO public.authors (first_name, last_name) VALUES
+      ('Donald', 'Knuth'),
+      ('Benoit', 'Mandelbrot');
+  END IF;
+
+  -- check publishers
+  SELECT COUNT(*) INTO records FROM public.publishers;
+  IF records = 0 THEN
+    INSERT INTO public.publishers (name) VALUES
+      ('Addison-Wesley Professional'),
+      ('W H Freeman & Co');
+  END IF;
+
+  -- check books
+  SELECT COUNT(*) INTO records FROM public.books;
+  IF records = 0 THEN
+   INSERT INTO public.books (name, isbn, author, publisher) VALUES
+    ('The Art of Computer Programming. Vol. 1', '978-0-201-89683-1', 1, 1),
+    ('The Fractal Geometry of Nature', '0-7167-1186-9', 2, 2);
+  END IF;
+
+ END
 $do$;
